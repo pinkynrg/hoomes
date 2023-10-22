255c35b7da3412f03c9bb4d00ac558e23ae91109
Francesco Meli <francesco.meli@beefree.io>
2023-10-22 09:00:16 +0200

    fix: remove broken import last commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { QueryClient, QueryClientProvider } from 'react-query';
 import { HomesList } from './components/HomesList/HomesList'
 import { EmptyState } from './components/EmptyState/EmptyState';
-import style from './App.module.scss';
 import { useState } from 'react';
 
 const queryClient = new QueryClient();
@@ -11,10 +10,8 @@ const App = () => {
   const [url, setUrl] = useState<string | undefined>()
   return (
     <QueryClientProvider client={queryClient}>
-      <div className={style.App}>
-        <HomesList onPreview={setUrl}/>
-        <EmptyState href={url ? 'http://localhost:5000/v1/proxy?url=' + url : undefined}/>
-      </div>
+      <HomesList onPreview={setUrl}/>
+      <EmptyState href={url ? 'http://localhost:5000/v1/proxy?url=' + url : undefined}/>
     </QueryClientProvider>
   )
 }
