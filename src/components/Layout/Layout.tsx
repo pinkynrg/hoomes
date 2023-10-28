10f8d43037701f0780e95e790d76e424ed8290cb
Francesco Meli <francesco.meli@beefree.io>
2023-10-28 19:29:15 +0200

    fix: now url is openable from mobile

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,15 +1,36 @@
 import { Outlet } from "react-router-dom";
 import style from "./Layout.module.scss";
+import { useSessionStorage } from "usehooks-ts";
+import Icon from '@ant-design/icons';
+import { Expander } from './../Icons/Expander';
 
-const Layout = () => (
-  <div className={style.Layout}>
-    <div className={style.Header}> 
-      <h1>Hoomes<span>.</span> </h1>
+const Layout = () => {
+  const [url, setUrl] = useSessionStorage<string | null>('url', null)
+  return (
+    <div className={style.Layout}>
+      <div className={style.Header}> 
+        <div className={style.Left}> 
+          { url ? 
+            <span className={style.Expander} onClick={() => setUrl(null)}>
+              <Icon component={Expander}/> 
+              <span> Back </span>
+            </span> : 
+            null 
+            }
+        </div>
+        <div className={style.Center}> 
+          <h1>
+            Hoomes<span>.</span>me
+          </h1>
+        </div>
+        <div className={style.Right}>
+        </div>
+      </div>
+      <div className={style.Body}>
+        <Outlet/>
+      </div>
     </div>
-    <div className={style.Body}>
-      <Outlet/>
-    </div>
-  </div>
-);
+  )
+};
 
 export { Layout }
--- src/pages/Listing/Listing.tsx
+++ src/pages/Listing/Listing.tsx
@@ -1,28 +1,30 @@
-import { useState } from "react";
 import { EmptyState } from "../../components/EmptyState/EmptyState";
 import { HomesList } from "../../components/HomesList/HomesList";
 import style from './Listing.module.scss';
-import { useLocalStorage } from "usehooks-ts";
+import { useLocalStorage, useSessionStorage } from "usehooks-ts";
 import { Home } from "../../types";
 import { Navigate } from "react-router-dom";
+import classnames from "classnames";
 
 const Listing = () => {
   const [homes] = useLocalStorage<Home[]>('data', [])
-  const [url, setUrl] = useState<string | undefined>()
+  const [url, setUrl] = useSessionStorage<string | null>('url', null)
 
   if (homes.length === 0) {
     return <Navigate to="/request" replace />
   }
 
   return (
-    <div className={style.Container}>
+    <div className={classnames(style.Container, {
+      [style.IsLoaded]: !!url
+    })}>
       <HomesList 
         homes={homes} 
         className={style.HomesList} 
         onPreview={setUrl}
       />
       <EmptyState 
-        className={style.EmptyState} 
+        className={classnames(style.EmptyState)} 
         href={url ? '/v1/proxy?url=' + url : undefined}
       />
     </div>
