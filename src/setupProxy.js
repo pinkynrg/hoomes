6547c4deb397befba452ac5ad1a6a8c752edd654
Francesco Meli <francesco.meli@beefree.io>
2023-10-28 19:04:20 +0200

    fix: development proxy

---
+++ src/setupProxy.js
@@ -0,0 +1,11 @@
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+module.exports = function(app) {
+  app.use(
+    '/v1',
+    createProxyMiddleware({
+      target: 'http://localhost:5000',
+      changeOrigin: true,
+    })
+  );
+};
