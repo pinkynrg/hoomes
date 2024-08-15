99a3e8e89db3709cbd48276f8cf2a13ea051494b
Francesco Meli <francesco.meli@beefree.io>
2024-08-15 15:00:49 +0200

    fix: remove obsolete record from sqlite

--- server/jobs.py
+++ server/jobs.py
@@ -7,12 +7,16 @@ from peewee import fn
 
 def fetch_homes(location: Location): 
     # Check if the lowest updated_at of House is > 1 day ago
-    one_day_ago = datetime.now() - timedelta(seconds=60)
+    now = datetime.now()
+    one_day_ago = now - timedelta(days=1)
     lowest_updated_at = House.select(fn.Min(House.updated_at)).where((House.city == location.nome) & (House.province == location.provincia_nome)).scalar()
 
     if lowest_updated_at is None or lowest_updated_at < one_day_ago:
         # Fetch data from Idealista
         data = Caasa.fetch(location)
+
+        # Keep track of the updated UUIDs
+        updated_uuids = []
 
         # Update the House records or insert new ones
         for item in data:
@@ -30,7 +34,16 @@ def fetch_homes(location: Location):
                 price=item['price'],
                 comment=item['comment'],
                 source=item['source'],
+                updated_at=now
             )
+            updated_uuids.append(item['uuid'])
+
+        # Delete records that were not updated
+        House.delete().where(
+            (House.city == location.nome) &
+            (House.province == location.provincia_nome) &
+            (House.uuid.not_in(updated_uuids))
+        ).execute()
 
     # Return all House records with comune equal to the input
     records = House.select().where((House.city == location.nome) & (House.province == location.provincia_nome))
