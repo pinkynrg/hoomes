c8c47fb07cfc95f4e5906187468954a7ebad86b7
Francesco Meli <francesco.meli@beefree.io>
2024-08-15 13:37:17 +0200

    fix: sizing house element in list

--- src/utils.ts
+++ src/utils.ts
@@ -2,7 +2,12 @@ const percentage = (floatValue: number) => `${(floatValue * 100).toFixed(2)}%`
 
 const NumberFormatter = Intl.NumberFormat('it-IT')
 
-const EuroFormatter = new Intl.NumberFormat('it-IT', { style: 'currency', currency: 'EUR' })
+const EuroFormatter = new Intl.NumberFormat('it-IT', {
+  style: 'currency',
+  currency: 'EUR',
+  minimumFractionDigits: 0,
+  maximumFractionDigits: 0,
+})
 
 const stringToNumber = (formattedNumber: string) => parseFloat(formattedNumber.replace(/[^\d]/g, ''))
 
